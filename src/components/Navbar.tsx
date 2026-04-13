7fa5595a5e4168acd1c6bf8f811c3c56b76bcc28
taantti <taan.kehitys@gmail.com>
2026-04-13 17:16:03 +0300

    feat: show/hide nav links based on user role permissions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,11 +21,17 @@ const NavBar = () => {
     return (
         <nav className="bg-white p-4 flex gap-4 items-center shadow-sm w-full">
             <NavLink to="/">Dashboard</NavLink>
-            <NavLink to="/products">Products</NavLink>
-            <NavLink to="/products/categories">Product Categories</NavLink>
-            <NavLink to="/users">Users</NavLink>
-            <button onClick={handleLogout}>Logout</button>  
-            <span className="text-gray-600">{user?.user_first_name} {user?.user_last_name}</span>  
+            {user?.rolePermission?.product?.readProducts?.access && (
+                <NavLink to="/products">Products</NavLink>
+            )}
+            {user?.rolePermission?.productCategory?.readProductCategories?.access && (
+                <NavLink to="/products/categories">Product Categories</NavLink>
+            )}
+            {user?.rolePermission?.user?.readUsers?.access && (
+                <NavLink to="/users">Users</NavLink>
+            )}
+            <button onClick={handleLogout}>Logout</button>
+            <span className="text-gray-600">{user?.user_first_name} {user?.user_last_name}</span>
         </nav>
     )
 }
